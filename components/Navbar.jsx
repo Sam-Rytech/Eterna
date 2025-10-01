cd580d1916be88e539b0cbdbafb8bb4468b5b599
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-02 00:35:14 +0100

    wne

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import '@/app/globals.css'
 
 const Navbar = () => {
   return (
-    <div className='bg-[url("/Hero/Hero.png")] bg-cover bg-center h-[900px]'>
-      <ul className="flex flex-end gap-5 justify-end">
-        <li>Home</li>
-        <li>About</li>
-        <li>Destination</li>
-        <li>Contact us</li>
-      </ul>
-    </div>
+    <nav className='bg-[url("/Hero/Hero.png")] bg-cover bg-center h-[900px] bg-no-repeat'>
+      <div>
+        <ul className="flex flex-end gap-5 justify-end w-[100%] list-none items-center">
+          <li className="Nav-li">
+            <a>Home</a>
+          </li>
+          <li className="Nav-li">
+            <a>About</a>
+          </li>
+          <li className="Nav-li">
+            <a>Destination</a>
+          </li>
+          <li className="Nav-li">
+            <a>Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
