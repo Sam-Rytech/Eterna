b97103ffb6709e5ece0e70d1835ccbabf07a9667
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-02 23:32:51 +0100

    bqw

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,47 +1,8 @@
-'use client'
-
 import React from 'react'
-import '@/app/globals.css'
-import { useState } from 'react'
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState(false)
   return (
-    <nav className='bg-[url("/Hero/Hero.png")] bg-cover bg-center h-[900px] bg-no-repeat'>
-      <div>
-        {/* Main Navbar */}
-        <ul className="flex flex-end gap-5 justify-end w-[100%] list-none items-center">
-          <li className="Nav-li">
-            <a>Home</a>
-          </li>
-          <li className="Nav-li">
-            <a>About</a>
-          </li>
-          <li className="Nav-li">
-            <a>Destination</a>
-          </li>
-          <li className="Nav-li">
-            <a>Contact us</a>
-          </li>
-        </ul>
-
-        {/* Side Navbar */}
-        <ul className="flex flex-end gap-5 justify-end w-[100%] list-none items-center">
-          <li className="Nav-li">
-            <a>Home</a>
-          </li>
-          <li className="Nav-li">
-            <a>About</a>
-          </li>
-          <li className="Nav-li">
-            <a>Destination</a>
-          </li>
-          <li className="Nav-li">
-            <a>Contact us</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div>Navbar</div>
   )
 }
 
