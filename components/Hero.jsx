0fe390b2140881ef64edd2c9460f1a8b224e0ad5
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-03 02:21:07 +0100

    bewh

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
 const Hero = () => {
-    return <section>
+    return (
+      <section>
         <div>
+          <div>
+            <h1 className="text-[#0B1956] text-6xl font-bold mb-5">
+              Escape. Indulge. Arrive.
+            </h1>
+          </div>
+          <div className='text-white'>
+            <div>Where every destination feels like a private masterpiece</div>
             <div>
-                <h1 className="text-white text-6xl font-bold mb-5">Escape. Indulge. Arrive.</h1>
+              <button>Discover Now</button>
             </div>
-            <div>
-                <div>Where every destination feels like a private masterpiece</div>
-                <div>
-                    <button>Discover Now</button>
-                </div>
-            </div>
-      </div>
-  </section>
+          </div>
+        </div>
+      </section>
+    )
 }
 
 export default Hero
