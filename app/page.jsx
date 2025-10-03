b37588cc7af309e01e89f95ed9081c154a7e7711
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-03 01:58:00 +0100

    jwe

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import "./globals.css"
 
 const page = () => {
   return (
-    <div className="bg-[#fdf9f6] text-gray-900">
+    <div>
       <Navbar />
     </div>
   )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../app/globals.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='flex justify-between'>
+        <div>
+          <h1>Eterna</h1>
+        </div>
+
+        <div>
+          <div className='burger'></div>
+        </div>
+        <div className='hidden'>
+          <ul className=''>
+            <li>Home</li>
+            <li>About</li>
+            <li>Destinations</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
