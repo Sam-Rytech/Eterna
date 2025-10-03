346e2fa9a455e3b2b70f5a1f0f9165b7413e0fbd
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-03 02:14:15 +0100

    beae

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,17 +3,19 @@ import "../app/globals.css"
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='flex justify-between'>
-        <div>
-          <h1>Eterna</h1>
+    <nav className="bg-[url('../public/Hero/Hero.png')] h-[900px] bg-cover bg-center bg-no-repeat">
+      <div className="flex justify-between mb-10">
+        <div className="h-20">
+          <h1 className="text-[#0B1956] h-[100%] flex items-center mx-15 my-5 text-5xl">
+            Eterna
+          </h1>
         </div>
 
         <div>
-          <div className='burger'></div>
+          <div className="burger"></div>
         </div>
-        <div className='hidden'>
-          <ul className=''>
+        <div className="hidden">
+          <ul className="">
             <li>Home</li>
             <li>About</li>
             <li>Destinations</li>
@@ -21,6 +23,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      <div className="border-b-2 border-[#696969] w-[90%] m-auto"></div>
     </nav>
   )
 }
