fbe7eec9e663f0b586af45d42ffedc624e6f3f08
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-09 16:17:28 +0100

    Updated the gradient of the image from white to transparent

--- components/Book.jsx
+++ components/Book.jsx
@@ -1,13 +1,34 @@
+'use client'
+
+import Image from 'next/image'
+import { ArrowRight, ArrowUp } from 'lucide-react'
 import React from 'react'
 
 const Book = () => {
   return (
-      <section>
-          <div>
-              <p>What are you waiting for ?</p>
-          </div>
-          <div> <a>Book Now <span></span> </a> </div>
-   </section>
+    <section className="w-full mt-10">
+      <div className="relative flex flex-col gap-20 items-center min-h-[687px] text-center">
+        <p className="text-4xl mt-15 text-[#11143D]">
+          What are you waiting for ?
+        </p>
+        <button className="flex items-center text-[#11143D] text-7xl font-semibold tracking-wide gap-3 hover:gap-5 transition-all">
+          BOOK NOW <ArrowRight className="w-10 h-10" />
+        </button>
+
+        {/* Background Image */}
+        <div className="absolute inset-0 -z-10">
+          <Image
+            src="/book/book.png"
+            alt="Tropical background"
+            fill
+            className="object-cover object-bottom"
+          />
+
+          {/* Gradient overlay */}
+          <div className="absolute inset-0 bg-gradient-to-b from-white via-white/80 to-transparent" />
+        </div>
+      </div>
+    </section>
   )
 }
 
