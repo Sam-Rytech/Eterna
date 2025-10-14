8c1b2d8298adf03107a2776e4e0201b00fd10208
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-14 01:34:31 +0100

    adjusted the responsiveness on mobile device and other screen size

--- components/Book.jsx
+++ components/Book.jsx
@@ -1,18 +1,22 @@
 'use client'
 
 import Image from 'next/image'
-import { ArrowRight, ArrowUp } from 'lucide-react'
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 const Book = () => {
   return (
-    <section className="w-full mt-10">
-      <div className="relative flex flex-col gap-20 items-center min-h-[687px] text-center">
-        <p className="text-4xl mt-15 text-[#11143D]">
-          What are you waiting for ?
+    <section className="relative w-full mt-10 overflow-hidden">
+      <div className="relative flex flex-col gap-10 md:gap-16 items-center justify-center min-h-[70vh] md:min-h-[80vh] text-center py-20">
+        {/* Text */}
+        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium text-[#11143D]">
+          What are you waiting for?
         </p>
-        <button className="flex items-center text-[#11143D] text-7xl font-semibold tracking-wide gap-3 hover:gap-5 transition-all">
-          BOOK NOW <ArrowRight className="w-10 h-10" />
+
+        {/* Button */}
+        <button className="flex items-center text-[#11143D] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold tracking-wide gap-2 sm:gap-3 hover:gap-5 transition-all duration-300">
+          BOOK NOW{' '}
+          <ArrowRight className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10" />
         </button>
 
         {/* Background Image */}
@@ -21,7 +25,8 @@ const Book = () => {
             src="/book/book.png"
             alt="Tropical background"
             fill
-            className="object-cover object-bottom"
+            priority
+            className="object-cover object-center md:object-bottom"
           />
 
           {/* Gradient overlay */}
